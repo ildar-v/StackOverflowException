bcc969a07b79223e0f1dffc824741fc6b7b7f03d
ildar04 <ildar04v@gmail.com>
2020-01-28 21:23:38 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+
+namespace StackOverflowException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            {
+                try
+                {
+                    var t1 = new TestStruct();
+                    var t2 = new TestStruct();
+                    var t3 = new TestStruct();
+                    var t4 = new TestStruct();
+                    var t5 = new TestStruct();
+                    var t6 = new TestStruct();
+                    var t7 = new TestStruct();
+                    var t8 = new TestStruct();
+                    var t9 = new TestStruct();
+                    var t10 = new TestStruct();
+                    var t11 = new TestStruct();
+                    var t12 = new TestStruct();
+                    var t13 = new TestStruct();
+                    var t14 = new TestStruct();
+                    var t15 = new TestStruct();
+                    var t16 = new TestStruct();
+                    var t17 = new TestStruct();
+                    var t18 = new TestStruct();
+                    var t19 = new TestStruct();
+                    var t20 = new TestStruct();
+                    var t21 = new TestStruct();
+                    var t22 = new TestStruct();
+                    var t23 = new TestStruct();
+                    var t24 = new TestStruct();
+                    var t25 = new TestStruct();
+                    var t26 = new TestStruct();
+                    var t27 = new TestStruct();
+                    var t28 = new TestStruct();
+                    var t29 = new TestStruct();
+                    var t30 = new TestStruct();
+                    var t31 = new TestStruct();
+                    var t32 = new TestStruct();
+                    var t33 = new TestStruct();
+                    var t34 = new TestStruct();
+                    var t35 = new TestStruct();
+                    var t36 = new TestStruct();
+                    var t37 = new TestStruct();
+                    var t38 = new TestStruct();
+                    var t39 = new TestStruct();
+                    var t40 = new TestStruct();
+                    var t41 = new TestStruct();
+                    var t42 = new TestStruct();
+                    var t43 = new TestStruct();
+                    var t44 = new TestStruct();
+                    var t45 = new TestStruct();
+                    var t46 = new TestStruct();
+                    var t47 = new TestStruct();
+                    var t48 = new TestStruct();
+                    var t49 = new TestStruct();
+                    var t50 = new TestStruct();
+                    var t51 = new TestStruct();
+                    var t52 = new TestStruct();
+                    var t53 = new TestStruct();
+                    var t54 = new TestStruct();
+                    var t55 = new TestStruct();
+                    var t56 = new TestStruct();
+                    var t57 = new TestStruct();
+                    var t58 = new TestStruct();
+                    var t59 = new TestStruct();
+                    var t60 = new TestStruct();
+                    var t61 = new TestStruct();
+                    var t62 = new TestStruct();
+                    var t63 = new TestStruct();
+                    var t64 = new TestStruct();
+                    var t65 = new TestStruct();
+                    var t66 = new TestStruct();
+                    var t67 = new TestStruct();
+                    var t68 = new TestStruct();
+                    var t69 = new TestStruct();
+                    var t70 = new TestStruct();
+                    var t71 = new TestStruct();
+                    var t72 = new TestStruct();
+                    var t73 = new TestStruct();
+                    var t74 = new TestStruct();
+                    var t75 = new TestStruct();
+                    var t76 = new TestStruct();
+                    var t77 = new TestStruct();
+                    var t78 = new TestStruct();
+                    var t79 = new TestStruct();
+                    var t80 = new TestStruct();
+                    var t81 = new TestStruct();
+                    var t82 = new TestStruct();
+                    var t83 = new TestStruct();
+                    var t84 = new TestStruct();
+                    var t85 = new TestStruct();
+                    var t86 = new TestStruct();
+                    var t87 = new TestStruct();
+                    var t88 = new TestStruct();
+                    var t89 = new TestStruct();
+                    var t90 = new TestStruct();
+                    var t91 = new TestStruct();
+                    var t92 = new TestStruct();
+                }
+                catch (Exception ex)
+                {
+                    System.Console.WriteLine(ex.Message);
+                }
+            }
+
+            System.Console.WriteLine("End.");
+            Console.ReadLine();
+        }
+    }
+}
